5f02a0dbe4fec9d868570e0d591b391fa4ee6d41
Eduardo Batista <dueduardoful@gmail.com>
2023-09-30 16:39:21 -0300

    subindo sidenav, icons, logo. ajustes css

--- pigeon/src/App.tsx
+++ pigeon/src/App.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import Header from './Componentes/Header'
 import Sidenav from './Componentes/Sidenav'
 import Resumo from './Pages/Resumo'
+import { DataContextProvider } from './Context/DataContext'
 
 const App = () => {
   return (
-    <div>
-      <Sidenav />
-      <main>
-        <Header />
-        <Resumo />
-      </main>
-    </div>
+    <DataContextProvider>
+      <div className='container'>
+        <Sidenav />
+        <main>
+          {/* <Header />
+          <Resumo /> */}
+        </main>
+      </div>
+    </DataContextProvider>
   )
 } 
 
