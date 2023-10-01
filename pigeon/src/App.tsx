03b306903d774a028b73e2ff8b3f5fe50574e352
Eduardo Batista <dueduardoful@gmail.com>
2023-09-30 23:11:35 -0300

    add navbar

--- pigeon/src/App.tsx
+++ pigeon/src/App.tsx
@@ -3,10 +3,12 @@ import Header from './Componentes/Header'
 import Sidenav from './Componentes/Sidenav'
 import Resumo from './Pages/Dashboard'
 import { DataContextProvider } from './Context/DataContext'
+import Navbar from './Componentes/Navbar'
 
 const App = () => {
   return (
     <DataContextProvider>
+      <Navbar />
       <div className='container'>
         <Sidenav />
         <main className='main'>
